a341396b4c9eaa56bf124c039129b61455f16bc2
miltonabdon <miltonabdon69@gmail.com>
2023-01-06 12:06:20 -0300

    first commit
    first commit

---
+++ src/com/ebac/HelloEbac.java
@@ -0,0 +1,9 @@
+package com.ebac;
+
+public class HelloEbac {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Ebac");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author miltonjunior
+ *
+ */
+module ebac {
+}
