2b86009bcfbfeeff7f466abc73d53cf5a2f3426a
miltonabdon <miltonabdon69@gmail.com>
2023-08-07 06:00:11 -0300

    comentarios
    adiçao de comentrarios

--- src/com/ebac/tarefa_classe/Carro.java
+++ src/com/ebac/tarefa_classe/Carro.java
@@ -1,15 +1,37 @@
 package com.ebac.tarefa_classe;
 
+/**
+ * @author miltonjunior
+ */
 public class Carro {
 
 	private String modelo;
 	private Integer velocidade;
 	private Float motor;
+	private boolean liga;
 
 	public Carro(String modelo, Float motor) {
 		this.modelo = modelo;
 		this.velocidade = 0;
 		this.motor = motor;
+		this.liga = false;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	protected boolean liga() {
+		return !liga;
+	}
+	
+	/**
+	 * 
+	 * @deprecated 
+	 * @see utilizar metodo liga() 
+	 */
+	protected boolean power() {
+		return true;
 	}
 
 	protected String acelera() {
